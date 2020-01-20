ff8f08619b8bd70b284fdb08b2dbc39d6b9378b3
zzy <1923791@qq.com>
2020-01-20 08:54:36 +0800

    Initial commit

---
+++ svc/src/test/java/com/zzy/svc/SvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
